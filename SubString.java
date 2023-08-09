992e0051a8de867931a6744835105d35d76b651c
Shra1B <141647864+Shra1B@users.noreply.github.com>
2023-08-09 13:53:01 +0530

    Create SubString.java

---
+++ SubString.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		String FullName="Shravan Balagoni";
+		String Name=FullName.substring(0,7);
+		System.out.println(Name);
+	}
+}
